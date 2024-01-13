f55fb6a797a3a550802c25f6dc0cb5df2987517e
JannaBiryukova <130726196+JannaBiryukova@users.noreply.github.com>
2024-01-13 05:52:21 +0300

    Initial commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,21 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+
+    public Long getNanoTime() {
+        return nanoTime;
+    }
+
+    public void setNanoTime(Long nanoTime) {
+        this.nanoTime = nanoTime;
+    }
+}
